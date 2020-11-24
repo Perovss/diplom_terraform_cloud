064481a4c26d14fc246b136c1cdfd6ebd55dff73
Перов Сергей <perov.ss@gmail.com>
2020-11-24 12:47:43 +0300

    111

--- main.tf
+++ main.tf
@@ -8,22 +8,24 @@ terraform {
 }
 data "aws_ami" "ubuntu" {
   most_recent = true
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
   owners = ["099720109477"] # Canonical
 }
-#
 resource "aws_instance" "web" {
   ami = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  tags = {
-  Name =aws_instance.web.instance_type
+   tags = {
+    Name = "First aws"
   }
 }
 data "aws_caller_identity" "current" {}
