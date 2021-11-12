e360949d495acfef44de35ef2c019ffa0d9f287e
sergey.perov <sergey.perov@soft-machine.ru>
2021-11-12 23:37:31 +0300

    update

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_instance" "diplom_instance" {
   count = 3
   subnet_id     = module.vpc.public_subnets[0]
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.small"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.diplom_sec_group.id]
   key_name= "diplom-key-pair"
 }
