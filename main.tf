f966956da62c5b65744b3fda95c4db855555b823
sergey.perov <sergey.perov@soft-machine.ru>
2021-11-13 01:48:37 +0300

    update

--- main.tf
+++ main.tf
@@ -84,16 +84,12 @@ resource "aws_security_group" "diplom_sec_group"{
     cidr_blocks = ["0.0.0.0/0"]
   }  
 }
-resource "aws_key_pair" "netology" {
-  key_name   = "netology"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCtofkxZqdUOafqSoMtSte1ND+Vw/4qWB5V9gMopKYSJadoR+PYUTcDcQ+TRCRSWfk58n4yEDHoMA1I+M6J13kC2wjh1P8Cwe39/9N+PV0JuJWC0+5Jxj4meGnqKmX0TbDhi2jxbN6PTL36wR9TpGh53URO0Y/lWnyH1DAhIgWwgo0SCTrkL93LDNF3D9rAEZAtasdmFnrFgciMa2i8be8eicORKwUWz9x+4WnSKc0xWlNp1qezUUAQloGTrR7c7aLXlplmQk+cQAqzEf2tMwbx/XAuYg4AjS16G+kjX7lfKz+GGJfgOcKRmRvi6VGHUxAPmXBMOrC5VZqnGONRnfNC99rCyMNNL/LTzdEGRW+KP4e331SBkA1C1WndV/9eSVQnSPvE7FFT906aDWyibQTYAi0x1AFOSN/FOSG/HkeKQqdQ0+y4JjOy3Xrye0KrHg41CPJ/Tx4t0bzHUB0zf7Kll3d8Y0tLxXH5kPRam2/k/wtJFMw0BeDY1u0j+FXTa3U= serge@Lenovo"
-}
 
 resource "aws_instance" "diplom_instance" {
-  count = 3
+  # count = 3
   subnet_id     = module.vpc.public_subnets[0]
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.diplom_sec_group.id]
-  key_name= "netology"
+  key_name  = aws_key_pair.netology.key_name
 }
