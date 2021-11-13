94a0f8f408d53f1cfe779c6005e525bde5f31e62
sergey.perov <sergey.perov@soft-machine.ru>
2021-11-14 01:48:45 +0300

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-2"
-  shared_credentials_file = "~/.aws/credentials"
-  profile = "default"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 data "aws_ami" "ubuntu" {
