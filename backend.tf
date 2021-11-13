729db94e4cae657e992ffe095aa24fd29305fe84
sergey.perov <sergey.perov@soft-machine.ru>
2021-11-13 22:43:22 +0300

    update

--- backend.tf
+++ backend.tf
@@ -1,5 +1,14 @@
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "perovss"
+
+    workspaces {}
   }
+}
+
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region     = var.AWS_DEFAULT_REGION
 }
