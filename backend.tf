61df40fe32922fac1a373f91670ec1e333ea5f1e
sergey.perov <sergey.perov@soft-machine.ru>
2021-11-13 22:59:38 +0300

    update

--- backend.tf
+++ backend.tf
@@ -6,9 +6,3 @@ terraform {
     workspaces {name = "stage"}
   }
 }
-
-provider "aws" {
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  region     = var.AWS_DEFAULT_REGION
-}
