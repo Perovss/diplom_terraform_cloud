30942cc29a44f3b9f2762a1a726b92a65cab8763
sergey.perov <sergey.perov@soft-machine.ru>
2021-11-15 17:31:54 +0300

    update

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ resource "aws_instance" "diplom_instance" {
   }
   subnet_id     = module.vpc.public_subnets[0]
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.small"
   vpc_security_group_ids = [aws_security_group.diplom_sec_group.id]
   key_name  = aws_key_pair.netology.key_name
 }
